5b7e7d9d4ec6c40d8d34b9d541bc7493987698e7
nvfp <nv-fp@users.noreply.github.com>
2024-04-21 11:18:22 -0700

    settings

--- nodes/ui/settings.gd
+++ nodes/ui/settings.gd
@@ -9,12 +9,13 @@ func _ready():
 	
 	for i in range(AudioServer.bus_count):
 		var bus = AudioServer.get_bus_volume_db(i)
-		var slider = get_node("MainMenuBG").get_node(names[i]).get_node("HSlider")
+		var slider = get_node("SettingsItems").get_node(names[i]).get_node("HSlider")
 		slider.value = bus
 		slider.connect("value_changed", change_volume.bind(slider, i))
 
 func change_volume(value: float, emitter, idx: int):
 	AudioServer.set_bus_volume_db(idx, value)
 
-func _back_eventc(viewport, event, shape_idx):
-	back.emit()
+func _back_event(viewport, event, shape_idx):
+	if InputUtil.is_click(event):
+		back.emit()
